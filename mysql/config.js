8efea5babb18d90205c4f04d14f7f76c4ae7d9b9
adem chaabani <77776710+adchaa@users.noreply.github.com>
2022-07-12 17:50:58 +0100

    Update config.js

--- mysql/config.js
+++ mysql/config.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const config = {
-  host: "fdb32.awardspace.net",
-  user: "4137503_taskmanager",
+  host: "mysql-82566-0.cloudclusters.net",
+  user: "admin",
   password: process.env.PASSWORD,
-  database: "4137503_taskmanager",
+  database: "task-manager",
+  port: 10356
 };
 module.exports = config;
